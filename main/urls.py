f80b943292d9da06fc56073e7c3e661ec71d0bc5
jongmin park <showppark12@naver.com>
2020-01-16 20:46:58 +0900

    jongmin

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+import lawboard.views 
+
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('lawboard/', include('lawboard.urls')),
   
 ]
